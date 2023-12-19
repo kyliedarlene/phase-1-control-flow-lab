277bc11df1a15273be3717f212ecdf187b7d8e94
Kylie Clark <kyliedarlene@icloud.com>
2023-12-19 16:30:48 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet) {
+  if (feet <= 400) {
+    return 'This one is on me!';
+  }
+  else if (feet > 2500) {
+    return 'No can do.';
+  }
+  else if (feet > 2000) {
+    return 'I will gladly take your thirty bucks.';
+  }
+  else {
+    return 'That will be twenty bucks.';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch(tip) {
+    case tip = "generous":
+      return "Thank you so much.";
+      break;
+    case tip = "not as generous":
+      return "Thank you.";
+      break;
+    default:
+      return "Bye.";
+      break;
+  }
 }
